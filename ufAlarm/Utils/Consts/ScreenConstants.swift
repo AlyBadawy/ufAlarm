cc7addb29c6541ef90906933e4f537a7af06d2b2
Aly Badawy <AlyBadawy@users.noreply.github.com>
2023-11-27 08:57:52 -0500

    Add Assets (Fonts, Icon, Consts, etc..)

---
+++ ufAlarm/Utils/Consts/ScreenConstants.swift
@@ -0,0 +1,11 @@
+//
+//  ScreenConstants.swift
+//  Alarm
+//
+//  Created by Aly Badawy on 11/26/23.
+//
+
+import SwiftUI
+
+let screenWidth = UIScreen.main.bounds.width
+let screenHeight = UIScreen.main.bounds.height
