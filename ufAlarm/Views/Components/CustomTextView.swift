c4fc30cc2036d75c732f95fc93ea5b1b0bea01ba
Aly Badawy <AlyBadawy@users.noreply.github.com>
2023-11-27 09:07:40 -0500

    Added the CustomTextView component

---
+++ ufAlarm/Views/Components/CustomTextView.swift
@@ -0,0 +1,26 @@
+//
+//  CustomTextView.swift
+//  ufAlarm
+//
+//  Created by Aly Badawy on 11/27/23.
+//
+
+import SwiftUI
+
+struct CustomTextView: View {
+  let fontName = "WorkSans-VariableFont_wght"
+  let text: String
+  let size: CGFloat
+  
+  var body: some View {
+    Text(text)
+      .font(Font.custom(fontName, size: size))
+      .shadow(color: .black.opacity(0.3),
+              radius: 3, x: 0, y: 0)
+  }
+}
+
+#Preview {
+    CustomTextView(text: "Add Alarm",
+                   size: 36)
+}
