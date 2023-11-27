dc2142570f4b0bfffb12064ed7050851ae33f90d
Aly Badawy <AlyBadawy@users.noreply.github.com>
2023-11-27 08:44:40 -0500

    Initial Commit

---
+++ ufAlarm/ufAlarmApp.swift
@@ -0,0 +1,17 @@
+//
+//  ufAlarmApp.swift
+//  ufAlarm
+//
+//  Created by Aly Badawy on 11/27/23.
+//
+
+import SwiftUI
+
+@main
+struct ufAlarmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
